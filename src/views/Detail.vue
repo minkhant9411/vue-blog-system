24f8ec417065743be9a670f11aceeab2dbc88b30
echo <aungminkhant@siriertech.com>
2023-09-06 11:55:07 +0630

    add password

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -44,7 +44,7 @@ export default {
     let { blog, error, load } = getBlog(props.id);
     load();
     let deleteBlog = async () => {
-      if (confirm("are you sure?")) {
+      if (pass === "mink1242022") {
         let res = doc(db, "blogs", props.id);
         deleteDoc(res);
         router.push("/");
